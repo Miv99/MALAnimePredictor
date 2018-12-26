a7d39c7e12ebc754d734f2a097542616286f3e89
Miv99 <treecam42@gmail.com>
2018-12-26 14:53:36 -0800

    Adjust user_collector logging
    Logging level changed to INFO and log file size limit set to 10mb.

--- user_collector.py
+++ user_collector.py
@@ -5,6 +5,19 @@ from mal_scraper import InvalidUserException
 from mal_info import User
 import pickle
 import logging
+from logging.handlers import RotatingFileHandler
+
+
+def configure_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    # Max 50mb log file
+    handler = RotatingFileHandler('logs/log.log', mode='a', maxBytes=10 * 1024 * 1024)
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
 
 
 def load_pickle_file(pickle_file, default={}):
@@ -18,10 +31,10 @@ def load_pickle_file(pickle_file, default={}):
 
 def save(users, anime_database, users_pickle_file_name, anime_database_pickle_file):
     with open(users_pickle_file_name + '.pickle', 'wb') as out:
-        logging.debug('Saving ' + str(len(users)) + ' unique users')
+        logging.info('Saving ' + str(len(users)) + ' unique users')
         pickle.dump(users, out, protocol=pickle.HIGHEST_PROTOCOL)
     with open(anime_database_pickle_file + '.pickle', 'wb') as out:
-        logging.debug('Saving ' + str(len(anime_database)) + ' unique anime')
+        logging.info('Saving ' + str(len(anime_database)) + ' unique anime')
         pickle.dump(anime_database, out, protocol=pickle.HIGHEST_PROTOCOL)
 
 
@@ -47,11 +60,11 @@ def collect_user_data(usernames_infile, anime_database_pickle_file_name, users_p
         dict of username as key and mal_info.User object as value
     autosave_period - number of user/anime requests before data is saved and backed-up
     """""
-    logging.basicConfig(filename='logs/log.log', filemode='a', level=logging.DEBUG,
-                        format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    logger = configure_logger()
+
     time_now = str(datetime.now()).replace('.', '_').replace('-', '_').replace(':', '_')
-    logging.debug('-------------------- ' + time_now + ' --------------------------')
-    logging.debug('usernames_infile = "' + usernames_infile +
+    logging.info('-------------------- ' + time_now + ' --------------------------')
+    logging.info('usernames_infile = "' + usernames_infile +
                   '"; anime_database_pickle_file = "' + anime_database_pickle_file_name
                   + '.pickle"; users_pickle_file = "' + users_pickle_file_name + '.pickle"')
 
@@ -73,54 +86,56 @@ def collect_user_data(usernames_infile, anime_database_pickle_file_name, users_p
         for username in usernames:
             print(username)
             if users.get(username) is None:
-                logging.debug('user_collector: Requesting info for user "' + username + '"')
+                logger.info('user_collector: Requesting info for user "' + username + '"')
                 autosave_counter += 1
                 try:
                     user = scraper.get_user(username)
                     for anime_id in user.anime_list.keys():
                         anime_database[anime_id] = None
                     users[username] = user
-                    logging.debug('user_collector: Retrieved info for user "' + username + '"')
+                    logger.info('user_collector: Retrieved info for user "' + username + '": ' + str(user))
 
                     if autosave_counter % autosave_period == 0:
                         save(users, anime_database, users_pickle_file_name, anime_database_pickle_file_name)
                         back_up(users, anime_database, users_pickle_file_name, anime_database_pickle_file_name)
                 except RequestException as e:
-                    logging.critical(e)
+                    logger.critical(e)
                 except InvalidUserException as e:
-                    logging.debug(e)
+                    logger.info(e)
                     users[username] = User(private_list_or_nonexistent=True)
                 except Exception as e:
-                    logging.fatal(e)
+                    logger.fatal(e)
+                    users[username] = User(private_list_or_nonexistent=True)
             elif users[username].private_list_or_nonexistent:
-                logging.debug('user_collector: Skipping user "' + username + '"; user does not exist or has private '
+                logger.info('user_collector: Skipping user "' + username + '"; user does not exist or has private '
                                                                              'anime list')
             else:
-                logging.debug('user_collector: Skipping user "' + username + '"; data already exists')
+                logger.info('user_collector: Skipping user "' + username + '"; data already exists')
 
         # Get info for all anime being used by the users
-        for anime_id in anime_database:
+        for anime_id in anime_database.keys():
             print(anime_id)
             if anime_database.get(anime_id) is None:
-                logging.debug('user_collector: Requesting info for anime id "' + anime_id + '"')
+                logger.info('user_collector: Requesting info for anime id "' + str(anime_id) + '"')
                 autosave_counter += 1
                 try:
-                    anime = scraper.get_anime(anime_id)
+                    anime = scraper.get_anime(str(anime_id))
                     anime_database[anime_id] = anime
-                    logging.debug('user_collector: Retrieved info for anime id "' + anime_id + '"')
+                    logger.info('user_collector: Retrieved info for anime id "' + str(anime_id) + '": ' + str(anime))
 
                     if autosave_counter % autosave_period == 0:
                         save(users, anime_database, users_pickle_file_name, anime_database_pickle_file_name)
                         back_up(users, anime_database, users_pickle_file_name, anime_database_pickle_file_name)
                 except RequestException as e:
-                    logging.critical(e)
+                    logger.critical(e)
                 except Exception as e:
-                    logging.fatal(e)
-
+                    logger.fatal(e)
             else:
-                logging.debug('user_collector: Skipping anime id "' + anime_id + '"; data already exists')
+                logger.info('user_collector: Skipping anime id "' + str(anime_id) + '"; data already exists')
     except KeyboardInterrupt:
-        logging.debug('user_collector: Keyboard-interrupted')
+        logger.info('user_collector: Keyboard-interrupted')
+    except Exception as e:
+        print(e)
 
     save(users, anime_database, users_pickle_file_name, anime_database_pickle_file_name)
     back_up(users, anime_database, users_pickle_file_name, anime_database_pickle_file_name)
